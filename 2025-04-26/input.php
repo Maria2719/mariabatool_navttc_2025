787d862709a3cb04f5b57a1b690bf534ee433af2
Maria2719 <121443395+Maria2719@users.noreply.github.com>
2025-05-03 10:20:16 +0500

    Add files via upload

---
+++ 2025-04-26/input.php
@@ -0,0 +1,20 @@
+
+<?php
+$num1= $_GET['a'];
+$num2= $_GET['b'];
+
+
+?>
+
+<form action=""  method="get">
+
+    <input type="text" name="a">
+    <input type="text" name="b">
+    <input type="submit" value="submit">
+
+</form>
+
+<h3>  Sum is <?= ($num1+ $num2) ?> </h3>
+<h3>  Difference is <?= ($num1- $num2) ?> </h3>
+<h3>  Product is <?= ($num1 * $num2) ?> </h3>
+<h3>  Quatient  is <?= ($num1 / $num2) ?> </h3>
