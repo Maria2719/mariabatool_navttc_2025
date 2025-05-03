fbd90fde03b48d8a783ec3a498663042fc0c0cc1
Maria2719 <121443395+Maria2719@users.noreply.github.com>
2025-05-03 10:12:39 +0500

    Add files via upload

---
+++ 2025-04-05/script.js
@@ -0,0 +1,19 @@
+
+
+
+let myName = "Abrar Hussain"
+let car = "Mehran"
+
+
+// Showing Output
+
+// document.body.innerText = "<h1>" + car + "</h1>"
+document.body.innerHTML = "<h1>" + car + "</h1>"
+
+// console.log( myName )
+// console.log( car )
+
+console.log( myName , "does not have" , car , "car"  )
+
+alert( myName + " " + car )
+confirm( myName + "concat"  )
