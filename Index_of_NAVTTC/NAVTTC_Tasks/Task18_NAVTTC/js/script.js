72caece850bf9cf34c0907b993e08ba1134dbe45
Maria2719 <121443395+Maria2719@users.noreply.github.com>
2025-05-23 06:12:17 +0500

    Delete Index_of_NAVTTC directory

--- Index_of_NAVTTC/NAVTTC_Tasks/Task18_NAVTTC/js/script.js
+++
@@ -1,6 +0,0 @@
-//18	Use Output Statement of JavaScript	Display your name on the screen using JavaScript.
-
-let firstName=" Maria "
-let secondName= "Batool"
-
-document.body.innerHTML=  firstName + secondName
